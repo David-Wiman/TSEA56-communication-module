3782fdad2ea748c42fe6a06fb363be9df3027245
David Wiman <davwi279@student.liu.se>
2022-03-31 17:25:45 +0200

    Kan skicka flera meddelanden efter varandra

--- test/client.py
+++ test/client.py
@@ -1,23 +1,24 @@
 import socket
-import threading
 
-HOST = '192.168.1.33'
-PORT = 1234
 
-def client():
-	threading.Timer(11, client).start()
-	
+# HOST = '192.168.1.33'
+# PORT = 1234
+
+def client(IP, PORT):
 	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-		s.connect((HOST, PORT))
+		s.connect((IP, PORT))
 		
-		request = input("Command...") + "\n"
-		encoded_request = request.encode('utf-8')
-		s.sendall(encoded_request)
-		
-		data = s.recv(1024).decode('utf-8')
-		print(data)
+		while (True):
+			# take input and send it
+			request = input("Command...") + "\n"
+			request = """{ "mapGraph": { "A1": [{"K1", 5}]}}""" + "\n"
+			encoded_request = request.encode('utf-8')
+			s.sendall(encoded_request)
+			
+			# recieve data and print it
+			data = s.recv(1024).decode('utf-8')
+			print(data)
 
 if __name__ == "__main__":
-	while True
-		client()
-		
+	while True:
+		client('192.168.1.33', 1234)
