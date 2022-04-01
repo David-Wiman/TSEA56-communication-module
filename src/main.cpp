305b58f4316a7ecaa0e84184698c829a7381c8a7
David Wiman <davwi279@student.liu.se>
2022-04-01 17:04:36 +0200

    added code to test new classes and methodes

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
+#include <json.hpp>
 
 #include "connection.h"
+#include "manualdriveinstruction.h"
 
 using namespace std;
+using json = nlohmann::json;
+
+bool exists(const json& j, const string& key) {
+    return j.find(key) != j.end();
+}
 
 int main() {
-    cout << "börjar" << endl;
+    cout << "Försöker etablera kontakt" << endl;
     Connection connection{1234};
-    cout << "klar" << endl;
+    cout << "Kontakt etablerad" << endl;
     while (true) {
-        cout << "redo att läsa" << endl;
+        cout << "Redo att läsa" << endl;
         string msg = connection.read();
-        cout << "färdigläst: " << msg << endl;
+        cout << "Mottaget: " << msg << endl;
+        cout << "Gör om till JSON-objekt" << endl;
+        json j = json::parse(msg);
+        cout << "JSON-objekt: " << j << endl;
+        bool is_ManualDriveInstruction = exists(j, "ManualDriveInstruction");
+        cout << "Finns key ManualDriveInstruction?: " << is_ManualDriveInstruction << endl;     
+        
+        if (is_ManualDriveInstruction) {
+            cout << "Försöker göra en instans av ManualDriveInstruction" << endl;
+            ManualDriveInstruction inst1(j);
+            cout << "Instansen skapades" << endl;
+            cout << "Throttle: " << inst1.get_throttle() << endl;
+            cout << "Steering: " << inst1.get_steering() << endl;
+        }
+        
         connection.write("ett svar");
     }
     return 0;
