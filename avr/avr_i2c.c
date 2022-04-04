928bdc06ba3dd2496908fc86d025b19bf09a890f
Albin Wahlén <alegow8@gmail.com>
2022-04-04 16:44:00 +0200

    Unpack i2c data

--- avr/avr_i2c.c
+++ avr/avr_i2c.c
@@ -2,49 +2,82 @@
 #include <avr/io.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <stdbool.h>
 
 // master writes: Slave Receive mode (SR)
 // master reads: Slave Transmit mode (ST)
 
 #define I2C_SR_START 0x60  // Own SLA+W has been received; ACK has been returned
-#define TEMP 0x68  // Arbitration lost in SLA+R/W as Master; own SLA+W has been received; ACK has been returned
-#define TEMP 0x70  // General call address has been received; ACK has been returned
-#define TEMP 0x78  // Arbitration lost in SLA+R/W as Master; General call address has been received; ACK has been returned
+//#define TEMP 0x68  // Arbitration lost in SLA+R/W as Master; own SLA+W has been received; ACK has been returned
+//#define TEMP 0x70  // General call address has been received; ACK has been returned
+//#define TEMP 0x78  // Arbitration lost in SLA+R/W as Master; General call address has been received; ACK has been returned
 #define I2C_SR_DATA 0x80  // Previously addressed with own SLA+W; data has been received; ACK has been returned
 #define I2C_SR_DATA_NACK 0x88  // Previously addressed with own SLA+W; data has been received; NOT ACK has been returned
-#define TEMP 0x90  // Previously addressed with general call; data has been re- ceived; ACK has been returned
-#define TEMP 0x98  // Previously addressed with general call; data has been received; NOT ACK has been returne
+//#define TEMP 0x90  // Previously addressed with general call; data has been re- ceived; ACK has been returned
+//#define TEMP 0x98  // Previously addressed with general call; data has been received; NOT ACK has been returne
 #define I2C_SR_STOP 0xA0  // A STOP condition or repeated START condition has been received while still addressed as Slave
+
+volatile uint8_t I2C_IN_BUFFER[32];
+volatile int I2C_IN_PTR = 0;
+volatile bool i2c_new_data = false;
+
+uint16_t message_names[16];
+uint16_t messages[16];
+
+
 
 void I2C_init() {
 	TWAR = 0x50 << 1;  // Set slave address
-	//TWCR = (1 << TWEA) | (1 << TWEN) | (1<<TWIE);
 	// Start Slave Listening: Clear TWINT Flag, Enable ACK, Enable TWI, TWI Interrupt Enable
 	TWCR = (1<<TWINT) | (1<<TWEA) | (1<<TWEN) | (1<<TWIE);
 
 	TWDR = 0x00;  // Default Initial Value
 }
 
+void I2C_unpack(uint16_t *message_names, uint16_t *messages) {
+	// The arrays must be long enough for all possible message types 
+	// Use length 16 to be safe
+	
+	int msg_idx = 0;
+	
+	// Translate the 8 bit packages to 16 bit data, and put them in the place for 
+	for (int i=0; i<I2C_IN_PTR/2; i++) {
+		uint16_t packet = (uint16_t)(I2C_IN_BUFFER[2*i] << 8) | I2C_IN_BUFFER[2*i+1];
+		printf(packet);
+		if ((packet & 0xfff0) == 0xfff0) {
+			// Message name
+			message_names[msg_idx] = packet;
+		} else {
+			// Message
+			messages[msg_idx++] = packet;
+		}
+	}
+	I2C_IN_PTR = 0;
+}
+
 ISR(TWI_vect) {
 	cli();
-	uint8_t status_code, newTWCR;
+	uint8_t status_code;
 	status_code = TWSR & 0xf8;
 	switch (status_code) {
 		case I2C_SR_START:
-		TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWIE);
-		break;
+			TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWIE);
+			break;
 		case I2C_SR_DATA:
-		TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWIE);
-		break;
+			I2C_IN_BUFFER[I2C_IN_PTR++] = TWDR;
+			TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWIE);
+			break;
 		case I2C_SR_STOP:
-		TWCR |= (1<<TWINT) | (1<<TWIE);
-		break;
+			//i2c_new_data = true;
+			I2C_unpack(message_names, messages);
+			TWCR |= (1<<TWINT) | (1<<TWIE);
+			break;
 		case I2C_SR_DATA_NACK:
-		printf("Error nack on data received\n");
-		break;
+			printf("Error nack on data received\n");
+			break;
 		default:
-		printf("Unrecognised status code\n");
-		break;
+			printf("Unrecognised status code\n");
+			break;
 	}
 	
 	sei();
@@ -63,7 +96,14 @@ int main(void){
 	I2C_init();
 
 	volatile int x = 0;
+	uint16_t message_names[16];
+	uint16_t messages[16];
+	
 	while (1) {
+		if (i2c_new_data) {
+			i2c_new_data = false;
+			I2C_unpack(message_names, messages);
+		}
 		x++;
 	}
 }
--- avr/message_constants.h
+++ avr/message_constants.h
@@ -2,13 +2,13 @@
 #define STEERING_CONSTANTS_H
 
 struct steering_struct {
-    const short int MANUAL_GAS = 0xfff0 | 0;
-    const short int MANUAL_ANG = 0xfff0 | 1;
-    const short int CUR_VEL    = 0xfff0 | 2;
-    const short int REF_VEL    = 0xfff0 | 3;
-    const short int CUR_LAT    = 0xfff0 | 4;
-    const short int REF_LAT    = 0xfff0 | 5;
-    const short int CUR_ANG    = 0xfff0 | 6;
-} STEERING;
+    const short int MANUAL_GAS;
+    const short int MANUAL_ANG;
+    const short int CUR_VEL;
+    const short int REF_VEL;
+    const short int CUR_LAT;
+    const short int REF_LAT;
+    const short int CUR_ANG;
+} STEERING = {0xfff0, 0xfff1, 0xfff2, 0xfff3, 0xfff4, 0xfff5, 0xfff6};
 
 #endif  // STEERING_CONSTANTS_H
