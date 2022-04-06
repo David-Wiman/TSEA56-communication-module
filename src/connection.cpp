6ff1a165d78a00b09bb91c7f3cc1cfe5e685d36d
David Wiman <davwi279@student.liu.se>
2022-04-06 11:28:34 +0200

    several getters and rewritten read-function

--- src/connection.cpp
+++ src/connection.cpp
@@ -1,15 +1,21 @@
-#include <string>
-#include <boost/asio.hpp>
+/* Written by David Wiman, 6/4 -22 */
 
 #include "connection.h"
 
 using namespace boost::asio;
 using ip::tcp;
 
+/* Check if a given key exists in an json object */ 
+bool exists(const json& j, const std::string& key) {
+    return j.find(key) != j.end();
+}
+
 /* Establish a connection on specified port, not done until a client respondes */
 Connection::Connection(int port)
-: port{port}, io_service{},
-  acceptor{io_service, tcp::endpoint(tcp::v4(), port)}, socket{io_service} {
+: port{port}, io_service{}, acceptor{io_service, tcp::endpoint(tcp::v4(),
+  port)},socket{io_service}, manual_instruction{false},
+  semi_instruction{false}, auto_instruction{false},
+  manual_drive_instruction{}, semi_drive_instruction{}, auto_drive_instruction{} {
       acceptor.accept(socket);
 }
 
@@ -21,16 +27,63 @@ void Connection::restart() {
       acceptor.accept(socket);  // waiting for connection
 }
 
-/* Recieve a string from the client */
-std::string Connection::read() {
-      boost::asio::streambuf buf;
-      boost::asio::read_until( socket, buf, "\n" );
-      std::string request = boost::asio::buffer_cast<const char*>(buf.data());
-      return request;
+/* Recieve a string from the client, set new_instruction, create instruction object */
+void Connection::read() {
+      try {
+            boost::asio::streambuf buf;
+            boost::asio::read_until( socket, buf, "\n" );
+            std::string request = boost::asio::buffer_cast<const char*>(buf.data());
+            json j = json::parse(request);
+            
+            if (exists(j, "ManualDriveInstruction")) {
+                  manual_instruction = true;
+                  ManualDriveInstruction inst{j};
+                  manual_drive_instruction = inst;
+            } else if (exists(j, "SemiDriveInstruction")) {
+                  semi_instruction = true;
+                  SemiDriveInstruction inst{j};
+                  semi_drive_instruction = inst;
+            } else if (exists(j, "AutoDriveInstruction")) {
+                  auto_instruction = true;
+                  AutoDriveInstruction inst{j};
+                  auto_drive_instruction = inst;
+            }
+      } catch (const boost::exception&) {
+            throw LostConnectionError();
+      }
 }
 
 /* Send a string to the client */
 void Connection::write(const std::string& response) {
       const std::string msg = response + "\n";
       boost::asio::write( socket, boost::asio::buffer(response));
+}
+
+/* New */
+bool Connection::new_manual_instruction() {
+      return manual_instruction;
+}
+
+bool Connection::new_semi_instruction() {
+      return semi_instruction;
+}
+
+bool Connection::new_auto_instruction() {
+      return auto_instruction;
+}
+
+/* Getters */
+ManualDriveInstruction Connection::get_manual_drive_instruction() {
+      manual_instruction = false;
+      return manual_drive_instruction;
+}
+
+SemiDriveInstruction Connection::get_semi_drive_instruction() {
+      semi_instruction = false;
+      return semi_drive_instruction;
+}
+
+AutoDriveInstruction Connection::get_auto_drive_instruction() {
+      auto_instruction = false;
+      return auto_drive_instruction;
 }
--- src/connection.h
+++ src/connection.h
@@ -3,20 +3,59 @@
 
 #include <string>
 #include <boost/asio.hpp>
+#include <json.hpp>
+
+#include "manualdriveinstruction.h"
+#include "semidriveinstruction.h"
+#include "autodriveinstruction.h"
+
+using json = nlohmann::json;
+
+/* Custom exception for connection loss */
+struct LostConnectionError : public std::exception
+{
+	const char * what () const throw ()
+    {
+    	return "Lost connection with user interface";
+    }
+};
 
 class Connection {
 public:
     Connection(int port);
-
+    
+    /* Basic function */
     void restart();
-    std::string read();
+    void read();
     void write(const std::string& response);
+    
+    /* New-functions */
+    bool new_manual_instruction();
+    bool new_semi_instruction();
+    bool new_auto_instruction();
+    
+    /* Getters */
+    ManualDriveInstruction get_manual_drive_instruction();
+    SemiDriveInstruction get_semi_drive_instruction();
+    AutoDriveInstruction get_auto_drive_instruction();
 
 private:
+    /* Variables for socket connection */
     int port;
     boost::asio::io_service io_service;
     boost::asio::ip::tcp::acceptor acceptor;
     boost::asio::ip::tcp::socket socket;
+    
+    /* Variables to check if a new instructions has come */
+    bool manual_instruction;
+    bool semi_instruction;
+    bool auto_instruction;
+    
+    /* Variables to return upon request */
+    ManualDriveInstruction manual_drive_instruction;
+    SemiDriveInstruction semi_drive_instruction;
+    AutoDriveInstruction auto_drive_instruction;
+
 };
 
 #endif  // CONNECTION_H
