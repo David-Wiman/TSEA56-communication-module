a07da245a8fc9ffa3e0ce8443540495bb474ecab
David Wiman <davwi279@student.liu.se>
2022-04-06 11:30:36 +0200

    cleanup and use of new functions for Connection

--- src/main.cpp
+++ src/main.cpp
@@ -8,48 +8,18 @@
 using namespace std;
 using json = nlohmann::json;
 
-bool exists(const json& j, const string& key) {
-    return j.find(key) != j.end();
-}
-
 int main() {
-    cout << "Försöker etablera kontakt" << endl;
     Connection connection{1234};
-    cout << "Kontakt etablerad" << endl;
-    while (true) {
-        cout << "Redo att läsa" << endl;
+    
+    while (true) {        
+        connection.read();
         
-        string msg{};
-        try {
-            msg = connection.read();
-        } catch (boost::exception&) {
-            cout << "Lost connection" << endl;
-            break;
+        if (connection.new_manual_instruction()) {
+            ManualDriveInstruction instruction = connection.get_manual_drive_instruction();
+            cout << "Recieved throttle: " << instruction.get_throttle() << endl;
+            cout << "Recieved steering: " << instruction.get_steering() << endl;
         }
-        
-        cout << "Mottaget: " << msg << endl;
-        cout << "Gör om till JSON-objekt" << endl;
-        
-        json j{};
-        try {
-            j = json::parse(msg);
-        } catch (std::invalid_argument&) {
-            cout << "Invalid argument" << endl;
-        }
-        
-        cout << "JSON-objekt: " << j << endl;
-        bool is_ManualDriveInstruction = exists(j, "ManualDriveInstruction");
-        cout << "Finns key ManualDriveInstruction?: " << is_ManualDriveInstruction << endl;     
-        
-        if (is_ManualDriveInstruction) {
-            cout << "Försöker göra en instans av ManualDriveInstruction" << endl;
-            ManualDriveInstruction inst1(j);
-            cout << "Instansen skapades" << endl;
-            cout << "Throttle: " << inst1.get_throttle() << endl;
-            cout << "Steering: " << inst1.get_steering() << endl;
-        }
-        
-        connection.write("ETT SVAR");
+        connection.write("Acknowledge");
     }
     return 0;
 }
