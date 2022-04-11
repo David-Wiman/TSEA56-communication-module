10c698ed1108030a1962d813849c1389609c10e2
Albin Wahlén <alegow8@gmail.com>
2022-04-11 18:12:19 +0200

    Revert "writing control parameters"
    This reverts commit 40725ffaad3530ec0354982f98cea9c64e697b3f.

--- src/main.cpp
+++ src/main.cpp
@@ -10,24 +10,20 @@ using namespace std;
 int main() {
     cout << "MAIN!" << endl;
     i2c_init();
-    i2c_set_slave_addr(0x51);
+    i2c_set_slave_addr(0x50);
 
     int16_t gas = 10;
     int16_t steer = -2;
-    int16_t steering_KP = 1200;
-    int16_t steering_KD = 1400;
-    int16_t speed_KP = 213;
-
     uint16_t buffer[] = {
         STEERING_MANUAL_GAS, package_signed(gas),
-        STEERING_MANUAL_ANG, package_signed(steer),
-        STEERING_STEERING_KP, package_signed(steering_KP),
-        STEERING_STEERING_KD, package_signed(steering_KD),
-        STEERING_SPEED_KP, package_signed(speed_KP)
+        STEERING_MANUAL_ANG, package_signed(steer)
     };
     uint16_t message_names[16];
     uint16_t messages[16];
-    i2c_write(buffer, 10);
+    int len = i2c_read(message_names, messages);
+    printf("Read %d packages:\n", len);
+    for (int i=0; i<len; ++i)
+        printf("\tName: 0x%x Data 0x%x\n", message_names[i], messages[i]);
     i2c_close();
     return 0;
 }
