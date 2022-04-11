089c08e8704461268d90f9032f564b894a8b0171
Albin Wahlén <alegow8@gmail.com>
2022-04-11 15:20:23 +0200

    Write from avr

--- common/avr_i2c.c
+++ common/avr_i2c.c
@@ -25,7 +25,8 @@ volatile int i2c_in_ptr = 0;
 volatile uint8_t i2c_out_buffer[32];
 volatile int i2c_out_ptr = 0;
 volatile int i2c_out_len = 0;
-volatile bool i2c_out_writing = false;
+volatile bool i2c_out_data_ready = false;
+volatile bool i2c_N_bytes_sent = false;
 
 void I2C_init(uint8_t slave_address) {
     TWAR = slave_address << 1;  // Set slave address
@@ -35,14 +36,27 @@ void I2C_init(uint8_t slave_address) {
     // Default initial values
     TWDR = 0x00;
     i2c_new_data = false;
-
-    i2c_out_buffer[0] = 0x12;
-    i2c_out_buffer[1] = 0x34;
-    i2c_out_buffer[2] = 0x56;
-    i2c_out_len = 3;
 }
 
-void I2C_pack(uint16_t *message_names, uint16_t *messages, int len) {}
+void I2C_pack(uint16_t *message_names, uint16_t *messages, int len) {
+    cli();
+	if (!i2c_out_data_ready) {
+		i2c_out_len = 0;
+		for (int i=0; i<len; ++i) {
+			uint8_t name0 = (uint8_t)((message_names[i] & 0xff00) >> 8);
+			uint8_t name1 = (uint8_t)(message_names[i] & 0x00ff);
+			uint8_t msg0 = (uint8_t)((messages[i] & 0xff00) >> 8);
+			uint8_t msg1 = (uint8_t)(messages[i] & 0x00ff);
+			i2c_out_buffer[4*i + 0] = name0;
+			i2c_out_buffer[4*i + 1] = name1;
+			i2c_out_buffer[4*i + 2] = msg0;
+			i2c_out_buffer[4*i + 3] = msg1;
+			i2c_out_len += 4;
+		}
+		i2c_out_data_ready = true;
+	}
+    sei();
+}
 
 int I2C_unpack(uint16_t *message_names, uint16_t *messages) {
     // The arrays must be long enough for all possible message types
@@ -90,10 +104,15 @@ ISR(TWI_vect) {
 
         // Slave Transmit mode
         case I2C_ST_START:
-            if (!i2c_out_writing) {
-                // Transmit number of bytes
-                TWDR = i2c_out_len;
-                TWCR |= (1<<TWINT) | (1<<TWIE);
+            if (!i2c_N_bytes_sent) {
+                if (i2c_out_data_ready) {
+                    // Transmit number of bytes
+                    TWDR = i2c_out_len;
+                    TWCR |= (1<<TWINT) | (1<<TWIE);
+                } else {
+                    TWDR = -2;
+                    TWCR |= (1<<TWINT) | (1<<TWIE);
+                }
             } else {
                 // Transmit first byte
                 i2c_out_ptr = 0;
@@ -111,7 +130,7 @@ ISR(TWI_vect) {
                 // Last byte
                 TWDR = i2c_out_buffer[i2c_out_ptr++];
                 TWCR |= (1<<TWINT) | (1<<TWIE);
-                //i2c_out_writing = false;
+                //i2c_N_bytes_sent = false;
             } else {
                 // This is unexpected
                 printf("ERROR\n");
@@ -119,11 +138,12 @@ ISR(TWI_vect) {
             break;
         case I2C_ST_WROTE_NACK:
             // Byte written, NACK received
-            if (!i2c_out_writing) {
-                i2c_out_writing = true;
+            if (!i2c_N_bytes_sent) {
+                i2c_N_bytes_sent = true;
                 TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWIE);
             } else if (i2c_out_ptr == i2c_out_len) {
-				i2c_out_writing = false;
+				i2c_N_bytes_sent = false;
+                i2c_out_data_ready = false;
                 TWCR |= (1<<TWINT) | (1<<TWEA) | (1<<TWIE);
             } else {
                 printf("ERROR\n");
--- common/test_main.c
+++ common/test_main.c
@@ -14,19 +14,24 @@ int main(void){
 
     I2C_init(0x50);
 
-    uint16_t message_names[16];
-    uint16_t messages[16];
-
-    I2C_pack(message_names, messages, 2);
+    uint16_t in_names[16];
+    uint16_t in_data[16];
+    uint16_t out_names[16];
+    uint16_t out_data[16];
 
     while (true) {
         if (i2c_new_data) {
             i2c_new_data = false;
-            int len = I2C_unpack(message_names, messages);
+            int len = I2C_unpack(in_names, in_data);
             for (int i=0; i<len; ++i) {
-                volatile int16_t val = restore_signed(messages[i]);
-                printf(message_names[i], val);
+                volatile int16_t val = restore_signed(in_data[i]);
+                printf(in_names[i], val);
             }
         }
+        out_names[0] = 0xfff0;
+        out_data[0] = 0x4321;
+        out_names[1] = 0xfff1;
+        out_data[1] = 0x8765;
+        I2C_pack(out_names, out_data, 2);
     }
 }
