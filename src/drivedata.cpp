67080ad4f71d15ddc223f2ba68efed41aaec7fb0
Victor Nossborn <vicno606@student.liu.se>
2022-04-19 14:02:57 +0200

    Updated drive data name to match user interface

--- src/drivedata.cpp
+++ src/drivedata.cpp
@@ -1,15 +1,15 @@
 #include "drivedata.h"
 
-DriveData::DriveData(int elasped_time, float throttle, float steering,
+DriveData::DriveData(int elasped_time, int throttle, int steering,
                     sensor_data_t sensor_data, camera_data_t camera_data)
-: elasped_time{elasped_time}, throttle{throttle}, steering{steering}, 
+: elapsed_time{elasped_time}, throttle{throttle}, steering{steering}, 
   obstacle_distance{sensor_data.obstacle_distance},
   driving_distance{sensor_data.driving_distance}, speed{sensor_data.speed},
   lateral_position{camera_data.lateral_position}, angle{camera_data.angle} {}
 
 std::string DriveData::format_json() {
     std::ostringstream oss;
-    oss << "{\"DriveData\": {\"time\": " << elasped_time
+    oss << "{\"DriveData\": {\"elapsed_time\": " << elapsed_time
         << ", \"throttle\": " << throttle
         << ", \"steering\": " << steering
         << ", \"obstacle_distance\": " << obstacle_distance
