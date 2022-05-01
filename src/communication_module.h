f56180640797744375e2d4a17d8277275283ad84
Alvin <alvgu648@student.liu.se>
2022-05-01 16:38:27 +0200

    Add declaration of get_steer_data in header

--- src/communication_module.h
+++ src/communication_module.h
@@ -13,6 +13,7 @@ public:
     CommunicationModule(int fps);
 
     static int get_sensor_data(sensor_data_t &sensor_data);
+    static int get_steer_data(steer_data_t &steer_data);
     static void send_manual_instruction(uint16_t throttle, uint16_t steering);
     static void send_auto_instruction(reference_t ref, int speed, int lateral_position);
     static void send_regulation_constants(
